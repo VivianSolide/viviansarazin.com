677123c8d860126c600cf6674699932271fe6531
VivianSolide <vivian.sarazin.dev@gmail.com>
2022-06-20 15:26:03 +0200

    feat: working on layouts

---
+++ pages/resume/index.vue
@@ -0,0 +1,65 @@
+<script lang='ts' setup>
+import Heading from '../../components/atoms/Heading.vue';
+import Position from '../../components/organisms/Position.vue';
+import Skills from '../../components/organisms/Skills.vue';
+import Skills1 from '../../components/organisms/Skills.vue';
+
+definePageMeta({
+  layout: "index",
+});
+
+const { data } = await useAsyncData('resume', () => queryContent('/resume').findOne())
+
+const computeDiffYears = (startDate: Date, endDate: Date) => {
+  const diff = endDate.getTime() - startDate.getTime()
+  return Math.floor(diff / (1000 * 60 * 60 * 24 * 365))
+}
+
+const now = new Date()
+const birthDate = new Date(1988, 10, 3)
+const ironHackStartDate = new Date(2017, 10, 1)
+
+const years = computeDiffYears(birthDate, now)
+const codingYears = computeDiffYears(ironHackStartDate, now)
+</script>
+<template>
+  <div>
+    <!-- Basics -->
+    <Heading heading="Vivian SARAZIN" class="pt-16" :order="1" />
+    <div class="grid md:grid-cols-2">
+      <div class="flex flex-col justify-around">
+        <Heading heading="Basics" />
+        <p>He/his</p>
+        <p>{{ years }} y/o</p>
+        <p>Living in {{ data.basics.location.city }}, France</p>
+        <p>Around {{ codingYears }} coding years</p>
+        <p>Looking for a Vue/Nuxt challenge</p>
+      </div>
+      <div>
+        <iframe style="border-radius:12px"
+          src="https://open.spotify.com/embed/playlist/0PlELr4gwTF9FeoDWzOs38?utm_source=generator&theme=0" width="100%"
+          height="380" frameBorder="0"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture">
+        </iframe>
+      </div>
+    </div>
+    <!-- Positions -->
+    <div class="grid grid-cols-1">
+      <Heading heading="Jobs" />
+      <Position :data="data.work" />
+      <Heading heading="Education" />
+      <Position :data="data.education" />
+    </div>
+    <!-- Skills -->
+    <div class="grid grid-cols-2">
+      <div>
+        <Heading heading="Skills" />
+        <Skills :data="data.skills" />
+      </div>
+      <div>
+        <Heading heading="Languages" />
+        <Skills :data="data.languages" />
+      </div>
+    </div>
+  </div>
+</template>
