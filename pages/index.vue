adf412867fc4eae7a93f11e7625047277d33db35
VivianSolide <vivian.sarazin.dev@gmail.com>
2022-06-20 15:26:28 +0200

    style: some layouts

--- pages/index.vue
+++ pages/index.vue
@@ -4,23 +4,20 @@ definePageMeta({
   layout: "index",
 });
 const { data } = await useAsyncData('hello', () => queryContent('/').findOne())
-
 </script>
 <template>
-  <div class="flex flex-col-reverse lg:flex-row lg:justify-between">
+  <div class="flex flex-col-reverse lg:flex-row lg:justify-between lg:pt-32">
     <div class="flex flex-col justify-center">
       <ContentRenderer :value="data" />
-      <div>
+      <div class="grid grid-cols-2 space-x-2">
         <NuxtLink to="/projects">
-          <Chip chip="See my projects" />
+          <Chip chip="See my projects" class="text-center" />
         </NuxtLink>
         <NuxtLink to="/resume">
-          <Chip chip="See my resume" />
+          <Chip chip="See my resume" class="text-center" />
         </NuxtLink>
       </div>
     </div>
-
-    <img src="~/assets/images/profile.jpeg" class="rounded-full" alt="" srcset="">
+    <img src="~/assets/images/profile.jpeg" class="lg:ml-8 rounded-full" alt="Vivian SARAZIN profile pic">
   </div>
-
 </template>
