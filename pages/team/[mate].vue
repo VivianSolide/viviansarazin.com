1902f0a62a8d043357ea894444ec3c6033835773
VivianSolide <vivian.sarazin.dev@gmail.com>
2022-07-01 11:41:26 +0200

    feat: michael & patrick

---
+++ pages/team/[mate].vue
@@ -0,0 +1,9 @@
+<script lang='ts' setup>
+const { mate } = useRoute().params
+</script>
+
+<template>
+  <div>
+    <ContentDoc :path="`/team/${mate}`" />
+  </div>
+</template>
---
+++ pages/team/index.vue
@@ -0,0 +1,19 @@
+<script lang='ts' setup>
+import Heading from '@/components/atoms/Heading.vue'
+const { data } = await useAsyncData('team', () => queryContent('/team').find())
+</script>
+
+<template>
+  <div>
+    <!-- Heading -->
+    <Heading heading="Teammates" :order="1" />
+    <!-- Team -->
+    <div class="grid md:grid-cols-2 md:gap-x-8 gap-y-2">
+      <div v-for="mate in data" :key="mate._path">
+        <NuxtLink :to="mate._path">
+          {{ mate.title }}
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
